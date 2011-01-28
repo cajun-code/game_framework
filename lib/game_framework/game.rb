ea33a8f8eb45609498fc6e8b28f9445d9c2595e0
Allan Davis <javaalley@gmail.com>
2011-01-27 20:40:31 -0500

    added game initializer

--- lib/game_framework/game.rb
+++ lib/game_framework/game.rb
@@ -1,12 +1,37 @@
+require 'rubygame'
 module GameFramework  
   class Game
     attr_reader :screen
     
-    def initialize(title, width, height, full_screen)
+    def initialize(title, width = 800, height = 600, full_screen = false, open_gl = false)
+      options = [Rubygame::HWSERFACE, Rubygame::DOUBLEBUF,]
+      options << Rubygame::FULLSCREEN if full_screen
+      options << 
+      @screen = Rubygame::Screen.new [width, height], 0, options 
+      @screen.title = title
       
+      @queue = Rubygame::EventQueue.new
+		  @clock = Rubygame::Clock.new
+		  @clock.target_framerate = 30
     end
     
     def start
+      
+    end
+    
+    def update_game
+      
+    end
+    
+    def draw_game
+      
+    end
+    
+    def draw
+      
+    end
+    
+    def update
       
     end
     
